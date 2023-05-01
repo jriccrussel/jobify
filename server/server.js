df712efeb309362df4863652ae7b41dad9e48cdb
jriccrussel <jriccrussel@gmail.com>
2023-05-01 20:33:54 +0800

    adding controllers, middlewares, routes, modification in the functionalities

--- server/server.js
+++ server/server.js
@@ -4,15 +4,29 @@ const app = express();
 import dotenv from 'dotenv'
 dotenv.config()
 
+// db
 import connectDB from './db/connect.js';
+
+// authenticate
+
+
+// router
+import authRouter from './routes/authRoutes.js';
+import jobsRouter from './routes/jobsRoutes.js';
+
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     // throw new Error('Error!!!')
     res.send('Welcome!')
 })
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
